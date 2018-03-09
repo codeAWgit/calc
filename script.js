43ca3ef7db23d0da0ad06e6717775374a390255a
codeAWgit <a_hobson@hotmail.com>
2018-03-09 15:19:22 -0600

    Worked for a brief period to get the order of operation working correctly. I need to take some time and pseudo-code before I work in the code more.

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
 'use strict'
-let operationInfo = { operator: '' }
+let operationInfo = { operator: '', entry: '0' }
 
 function clrEntry(alsoNums) {
     $('.calcScreen').text('0')
     if (alsoNums) {
-        operationInfo = { operator: '' }
+        operationInfo = { operator: '', entry: '0' }
     }
 }
 
 function numToScreen(scrNum) {
-    let crntScreen = document.getElementsByClassName('calcScreen')[0].innerHTML
-    crntScreen += scrNum
+   operationInfo.entry += scrNum
     
-    crntScreen = crntScreen.replace(/[.]+/, '.')
-    $('.calcScreen').text(crntScreen.replace(/^[0]([1-9])/, '$1'))
+    operationInfo.entry = operationInfo.entry.replace(/[.]+/, '.')
+    operationInfo.entry = operationInfo.entry.replace(/^[0]([1-9])/, '$1')
+    // Decide on a way to get rid of additional places where zeroes can be added.
+    $('.calcScreen').text( operationInfo.entry )
+    console.log(operationInfo)
 }
 
 function operation(oprtr) {
     if ( !operationInfo.operator ) {
-        operationInfo.frstOperand = +document.getElementsByClassName('calcScreen')[0].innerHTML
+        operationInfo.frstOperand = +operationInfo.entry
         operationInfo.operator = oprtr
+        operationInfo.entry = '0'
         clrEntry()
-    }
-    else if ( operationInfo.operator == 'equal' ){
-        //let scndOperand = document.getElementsByClassName('calcScreen')[0].innerHTML
-        clrEntry()
-        numToScreen( (Number(operationInfo.frstOperand) + Number(scndOperand)).toString() )
-        operationInfo.frstOperand = scndOperand
-        operationInfo.operator = oprtr  
     }
     else if ( operationInfo.operator == 'plus' ){
-        let scndOperand = +document.getElementsByClassName('calcScreen')[0].innerHTML
-        let result = operationInfo.frstOperand + scndOperand
+        let result = operationInfo.frstOperand + Number(operationInfo.entry)
+
+        operationInfo.entry = '0'
+        clrEntry()
+        numToScreen( result.toString() )
+        operationInfo.frstOperand = result
+        operationInfo.operator = oprtr  
+    }
+    else if ( operationInfo.operator == 'equal' ){
+        let result = operationInfo.frstOperand + Number(operationInfo.entry)
         clrEntry()
         numToScreen( result.toString() )
         operationInfo.frstOperand = result
         operationInfo.operator = oprtr  
     }
 }
+
+/*
+
+operand is entered (key presses result in new numbers being displayed on the screen)
+
+operator is entered (screen shows zero as its ready to receive another operand, operator is saved)
+
+second operand is entered (key presses show on the screen for the second operand)
+
+another operation is entered or equal is entered
+
+if equal then that displays the result and resets any saved values
+if another operation then the result is shown and another number will be taken to perform that operation
+
+*/
